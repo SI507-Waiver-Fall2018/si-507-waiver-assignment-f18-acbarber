d55d9228c5bbcf09c77d511d15f6f67925bd9992
acbarber <40249854+acbarber@users.noreply.github.com>
2018-06-22 22:35:32 -0400

    Add files via upload

--- part2.py
+++ part2.py
@@ -7,7 +7,7 @@ d = data.cursor()
 
 
 def customers():
-    d.execute('SELECT Id, ContactName FROM Customer')
+    d.execute('SELECT Id, CompanyName FROM Customer')
     customer_list=d.fetchall()
     print ('ID      Customer Name')
     for pair in customer_list:
@@ -27,7 +27,7 @@ def orders(cust='o.CustomerId', emp='e.LastName'):
     order_dates=d.fetchall()
     print('Order Dates')
     for date in order_dates:
-        print(date)
+        print(date[0])
     return None
 
 
@@ -38,7 +38,7 @@ try:
     elif __name__=="__main__" and sys.argv[1]=='orders' and sys.argv[2][:4]=='cust':
         orders(cust=sys.argv[2][5:])
     elif __name__=="__main__" and sys.argv[1]=='orders' and sys.argv[2][:3]=='emp':
-        orders(emp=sys.argv[2][4:])
+        orders(emp=str(sys.argv[2][4:]))
     elif __name__=="__main__" and sys.argv[1]=='employees':
         employees()
 except IndexError:
